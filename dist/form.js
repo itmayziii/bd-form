020e6e265f650acfcbda1e2bc0ba7c0b2e564774
itmayziii <tommymay37@gmail.com>
2017-12-11 09:56:58 -0500

    Removed the legacy form class

--- dist/form.js
+++ dist/form.js
@@ -154,13 +154,10 @@ module.exports = __webpack_require__(2);
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__form__ = __webpack_require__(3);
-/* harmony namespace reexport (by provided) */ __webpack_require__.d(__webpack_exports__, "Form", function() { return __WEBPACK_IMPORTED_MODULE_0__form__["a"]; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__form_builder__ = __webpack_require__(4);
-/* harmony namespace reexport (by provided) */ __webpack_require__.d(__webpack_exports__, "BdFormBuilder", function() { return __WEBPACK_IMPORTED_MODULE_1__form_builder__["a"]; });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__validators_group_validators__ = __webpack_require__(7);
-/* harmony namespace reexport (by provided) */ __webpack_require__.d(__webpack_exports__, "GroupValidators", function() { return __WEBPACK_IMPORTED_MODULE_2__validators_group_validators__["a"]; });
-
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__form_builder__ = __webpack_require__(3);
+/* harmony namespace reexport (by provided) */ __webpack_require__.d(__webpack_exports__, "BdFormBuilder", function() { return __WEBPACK_IMPORTED_MODULE_0__form_builder__["a"]; });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__validators_group_validators__ = __webpack_require__(6);
+/* harmony namespace reexport (by provided) */ __webpack_require__.d(__webpack_exports__, "GroupValidators", function() { return __WEBPACK_IMPORTED_MODULE_1__validators_group_validators__["a"]; });
 
 
 
@@ -170,216 +167,8 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-class Form {
-    constructor(formSelector, document, classPrefix = 'bd') {
-        this._originalFormValues = {};
-        this._validFormInputs = [
-            'INPUT', 'SELECT', 'TEXTAREA'
-        ];
-        this._formInputStates = {
-            TOUCHED: '-touched',
-            UNTOUCHED: '-untouched',
-            PRISTINE: '-pristine',
-            DIRTY: '-dirty',
-            INVALID: '-invalid',
-            VALID: '-valid'
-        };
-        this._formStates = {
-            PRISTINE: '-pristine',
-            DIRTY: '-dirty',
-            INVALID: '-invalid',
-            VALID: '-valid',
-            SUBMITTED: '-submitted'
-        };
-        this._document = document;
-        this._setClassPrefix(classPrefix);
-        this._attachToDom(formSelector);
-        this._setOriginalFormValues();
-    }
-    getValues() {
-        const formValues = {};
-        this._retrieveFormInputs().forEach((formInput) => {
-            formValues[formInput.getAttribute('name')] = formInput.value;
-        });
-        return formValues;
-    }
-    disable() {
-        this._retrieveFormInputs().forEach((formInput) => {
-            formInput.setAttribute('disabled', 'true');
-        });
-    }
-    enable() {
-        this._retrieveFormInputs().forEach((formInput) => {
-            formInput.removeAttribute('disabled');
-        });
-    }
-    isValid() {
-        return this._form.checkValidity();
-    }
-    _attachToDom(formSelector) {
-        this._form = HTMLFormElement = this._document.querySelector(formSelector);
-        this._addInputListeners();
-        this._resetFormClasses();
-        this._resetInputClasses();
-    }
-    _resetFormClasses() {
-        this._form.classList.remove(this._formStates.DIRTY);
-        this._form.classList.remove(this._formStates.SUBMITTED);
-        if (this._form.checkValidity()) {
-            this._form.classList.remove(this._formStates.INVALID);
-            this._form.classList.add(this._formStates.VALID);
-        }
-        else {
-            this._form.classList.remove(this._formStates.VALID);
-            this._form.classList.add(this._formStates.INVALID);
-        }
-        this._form.classList.add(this._formStates.PRISTINE);
-    }
-    _resetInputClasses() {
-        this._retrieveFormInputs().forEach((formInput) => {
-            const inputClassEl = this._getInputClassElement(formInput);
-            inputClassEl.classList.remove(this._formInputStates.DIRTY);
-            inputClassEl.classList.add(this._formInputStates.PRISTINE);
-            inputClassEl.classList.remove(this._formInputStates.TOUCHED);
-            inputClassEl.classList.add(this._formInputStates.UNTOUCHED);
-            if (formInput.checkValidity()) {
-                inputClassEl.classList.remove(this._formInputStates.INVALID);
-                inputClassEl.classList.add(this._formInputStates.VALID);
-            }
-            else {
-                inputClassEl.classList.remove(this._formInputStates.VALID);
-                inputClassEl.classList.add(this._formInputStates.INVALID);
-            }
-        });
-    }
-    _addInputListeners() {
-        this._retrieveFormInputs().forEach((formInput) => {
-            formInput.addEventListener('blur', (event) => {
-                this._onInputBlur(event.currentTarget);
-            });
-            const formInputType = formInput.getAttribute('type');
-            if (formInputType === 'checkbox' || formInputType === 'radio') {
-                formInput.addEventListener('change', (event) => {
-                    this._onInputChange(event.target);
-                });
-            }
-            else {
-                formInput.addEventListener('keyup', (event) => {
-                    this._onInputChange(event.target);
-                });
-            }
-        });
-    }
-    _onInputBlur(target) {
-        this._updateValidTouchedInputClasses(target);
-    }
-    _onInputChange(target) {
-        this._updatePristineInputClasses(target);
-        this._updateFormPristineClass();
-    }
-    _updatePristineInputClasses(formInput) {
-        const formInputValue = this.getInputValue(formInput);
-        const inputName = formInput.getAttribute('name');
-        const inputClassEl = this._getInputClassElement(formInput);
-        if (this._originalFormValues[inputName] === formInputValue) {
-            inputClassEl.classList.remove(this._formInputStates.DIRTY);
-            inputClassEl.classList.add(this._formInputStates.PRISTINE);
-        }
-        else {
-            inputClassEl.classList.remove(this._formInputStates.PRISTINE);
-            inputClassEl.classList.add(this._formInputStates.DIRTY);
-        }
-    }
-    _updateFormPristineClass() {
-        const nonPristineInput = this._retrieveFormInputs().find((formInput) => {
-            const formInputValue = this.getInputValue(formInput);
-            const formInputName = formInput.getAttribute('name');
-            return formInputValue !== this._originalFormValues[formInputName];
-        });
-        if (nonPristineInput) {
-            this._form.classList.remove(this._formStates.PRISTINE);
-            this._form.classList.add(this._formStates.DIRTY);
-        }
-        else {
-            this._form.classList.remove(this._formStates.DIRTY);
-            this._form.classList.add(this._formStates.PRISTINE);
-        }
-    }
-    _updateValidTouchedInputClasses(formInput) {
-        const inputClassEl = this._getInputClassElement(formInput);
-        inputClassEl.classList.remove(this._formInputStates.UNTOUCHED);
-        inputClassEl.classList.add(this._formInputStates.TOUCHED);
-        if (formInput.checkValidity()) {
-            inputClassEl.classList.remove(this._formInputStates.INVALID);
-            inputClassEl.classList.add(this._formInputStates.VALID);
-        }
-        else {
-            inputClassEl.classList.remove(this._formInputStates.VALID);
-            inputClassEl.classList.add(this._formInputStates.INVALID);
-        }
-    }
-    _retrieveFormInputs() {
-        const formInputs = [];
-        const formControls = this._form.elements;
-        for (let i = 0; i < formControls.length; i++) {
-            const formControl = formControls[i];
-            if (this._validFormInputs.indexOf(formControl.tagName) === -1) {
-                continue;
-            }
-            formInputs.push(formControl);
-        }
-        return formInputs;
-    }
-    _setOriginalFormValues() {
-        this._retrieveFormInputs().forEach((formInput) => {
-            const formInputName = formInput.getAttribute('name');
-            this._originalFormValues[formInputName] = this.getInputValue(formInput);
-        });
-    }
-    // noinspection JSMethodCanBeStatic
-    _getInputClassElement(formInput) {
-        let inputClassEl;
-        if (formInput.parentElement.tagName !== 'FORM') {
-            inputClassEl = formInput.parentElement;
-        }
-        else {
-            inputClassEl = formInput;
-        }
-        return inputClassEl;
-    }
-    // noinspection JSMethodCanBeStatic
-    getInputValue(input) {
-        const inputType = input.getAttribute('type');
-        let inputValue = input.value;
-        if ((inputType === 'checkbox' || inputType === 'radio') && input.checked === true) {
-            inputValue = (input.checked) ? 'on' : 'off';
-        }
-        return inputValue;
-    }
-    _setClassPrefix(classPrefix) {
-        for (let inputState in this._formInputStates) {
-            if (this._formInputStates.hasOwnProperty(inputState)) {
-                this._formInputStates[inputState] = `${classPrefix}${this._formInputStates[inputState]}`;
-            }
-        }
-        for (let formState in this._formStates) {
-            if (this._formStates.hasOwnProperty(formState)) {
-                this._formStates[formState] = `${classPrefix}${this._formStates[formState]}`;
-            }
-        }
-    }
-}
-/* harmony export (immutable) */ __webpack_exports__["a"] = Form;
-
-
-
-/***/ }),
-/* 4 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__form_control__ = __webpack_require__(5);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__form_group__ = __webpack_require__(6);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__form_control__ = __webpack_require__(4);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__form_group__ = __webpack_require__(5);
 
 
 class BdFormBuilder {
@@ -407,7 +196,7 @@ class BdFormBuilder {
 
 
 /***/ }),
-/* 5 */
+/* 4 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -565,7 +354,7 @@ class BdFormControl extends __WEBPACK_IMPORTED_MODULE_0__abstract_control__["a" 
 
 
 /***/ }),
-/* 6 */
+/* 5 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
@@ -715,7 +504,7 @@ class BdFormGroup extends __WEBPACK_IMPORTED_MODULE_0__abstract_control__["a" /*
 
 
 /***/ }),
-/* 7 */
+/* 6 */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
-export * from './form';
 export * from './form-builder';
 export * from './validators/group-validators';
